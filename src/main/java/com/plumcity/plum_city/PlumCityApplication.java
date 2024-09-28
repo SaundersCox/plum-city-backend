d745c04762b5f71771049ae6319b34ac6557604f
saunderscox <saunderscox@gmail.com>
2024-09-28 11:25:43 -0400

    init spring boot app

---
+++ src/main/java/com/plumcity/plum_city/PlumCityApplication.java
@@ -0,0 +1,13 @@
+package com.plumcity.plum_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plumcity/plum_city/PlumCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plumcity.plum_city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
