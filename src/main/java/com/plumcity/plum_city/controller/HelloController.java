4f80c7e7855cc8b901ea6c6796b0de678ad34ee1
saunderscox <saunderscox@gmail.com>
2024-10-15 22:18:08 -0400

    working backend

--- src/main/java/com/plumcity/plum_city/controller/HelloController.java
+++ src/main/java/com/plumcity/plum_city/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.plumcity.plum_city.templates;
+package com.plumcity.plum_city.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
