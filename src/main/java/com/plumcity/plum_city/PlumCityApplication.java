777002f82a62d52feebce63dde83241e82d331c0
saunderscox <saunderscox@gmail.com>
2024-10-20 19:11:09 -0400

    removed tests

--- src/main/java/com/plumcity/plum_city/PlumCityApplication.java
+++ src/main/java/com/plumcity/plum_city/PlumCityApplication.java
@@ -9,12 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlumCityApplication {
 
   public static void main(String[] args) {
-    log.info("Environment Variables");
-    System.getenv()
-        .forEach(
-            (key, value) -> {
-              log.info(key + ": " + value);
-            });
     SpringApplication.run(PlumCityApplication.class, args);
   }
 }
