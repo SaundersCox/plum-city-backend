678dc15bbeed433f5ebc9b8e8751a813b213edd4
Daniel Cox <Daniel.Cox@estes-express.com>
2024-12-31 15:49:10 -0500

    updated for deploy

--- src/main/java/com/plumcity/plum_city/controller/HelloController.java
+++ src/main/java/com/plumcity/plum_city/controller/HelloController.java
@@ -1,15 +1,16 @@
 package com.plumcity.plum_city.controller;
 
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-  @GetMapping("/greet")
-  public String greet(Model model) {
-    model.addAttribute("message", "Hello, Thymeleaf!");
-    return "greet";
+  @GetMapping
+  public ResponseEntity<String> echo(@RequestParam(defaultValue = "dude") String name) {
+    return new ResponseEntity<>("Welcome " + (name == null ? "" : name), HttpStatus.OK);
   }
 }
